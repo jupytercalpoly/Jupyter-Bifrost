2303b8a737cd3f9fe5a1f30f065152ace79b198a
jahn <aju960219@gmail.com>
2021-07-07 12:33:46 -0700

    Removed unnecessary comments

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -3,10 +3,7 @@ import { jsx } from '@emotion/react';
 import { VegaLite } from 'react-vega';
 import { useModelState, GraphData, GraphSpec } from '../hooks/bifrost-model';
 
-interface GraphProps {
-  onBack?: () => void;
-}
-export default function Graph(props: GraphProps) {
+export default function Graph() {
   const [selectedData, setSelectedData] = useModelState<any[]>('selected_data');
   const spec = useModelState<GraphSpec>('graph_spec')[0];
 
@@ -19,20 +16,11 @@ export default function Graph(props: GraphProps) {
 
   const data = useModelState<GraphData>('graph_data', (data) => ({ data }))[0];
 
-  // let back_button: JSX.Element | null = null;
-  // if (props.onBack) {
-  //   back_button = (
-  //     <button className="wrapper" onClick={props.onBack}>
-  //       <ArrowLeft />
-  //     </button>
-  //   );
-  // }
   // multiple signals can be added by adding a new field
   const signalListeners = { brush: handleBrush };
 
   return (
     <div>
-      {/* {back_button} */}
       <VegaLite spec={spec} data={data} signalListeners={signalListeners} />
     </div>
   );
