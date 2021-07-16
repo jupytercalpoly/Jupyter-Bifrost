e566b21094edd83acbbdf635d67e03ea9ca02c1b
jahn <aju960219@gmail.com>
2021-07-16 09:58:37 -0700

    Fixed bifrost style affecting jupyterlab style

--- src/components/BifrostReactWidget.tsx
+++ src/components/BifrostReactWidget.tsx
@@ -15,45 +15,58 @@ import VisualizationScreen from './VisualizationScreen';
 const globalStyles = (theme: any) => css`
   // Global styles for the widget
   //===========================================================
-  * {
-    box-sizing: border-box;
-  }
-
-  button {
-    cursor: pointer;
-    transition: transform 0.4s;
-    background-color: ${theme.color.primary.dark};
-    color: white;
-    font-weight: 700;
-    padding: 10px 15px;
-    border-radius: 7px;
-    font-size: 16px;
-    border: none;
-
-    &:active {
-      transform: scale(0.95);
+  .bifrost-widget {
+    * {
+      box-sizing: border-box;
     }
 
-    &.wrapper {
+    button {
+      cursor: pointer;
+      transition: transform 0.4s;
+      background-color: ${theme.color.primary.dark};
+      color: white;
+      font-weight: 700;
+      padding: 10px 15px;
+      border-radius: 7px;
+      font-size: 16px;
       border: none;
-      background: transparent;
-      margin: 0;
-      padding: 0;
-      margin-right: 15px;
-      color: initial;
+
+      &:active {
+        transform: scale(0.95);
+      }
+
+      &.wrapper {
+        border: none;
+        background: transparent;
+        margin: 0;
+        padding: 0;
+        margin-right: 15px;
+        color: initial;
+      }
+
+      &.next-button {
+        padding: 0;
+        background-color: ${theme.color.primary.dark};
+        border: none;
+        border-radius: 50%;
+        cursor: pointer;
+        height: 28px;
+        width: 28px;
+        margin-left: 20px;
+      }
     }
-  }
 
-  h1 {
-    font-size: 35px;
-    font-weight: 800;
-    margin: 10px 0;
-    margin-bottom: 15px;
-  }
+    h1 {
+      font-size: 35px;
+      font-weight: 800;
+      margin: 10px 0;
+      margin-bottom: 15px;
+    }
 
-  h2 {
-    font-size: 25px;
-    font-weight: 800;
+    h2 {
+      font-size: 25px;
+      font-weight: 800;
+    }
   }
 `;
 
