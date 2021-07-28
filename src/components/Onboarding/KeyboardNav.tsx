d87fe02912c2ba2861977aeb9071c5e160d7a7a4
John Waidhofer <waidhoferj@gmail.com>
2021-07-28 12:04:52 -0700

    added docstrings

--- src/components/Onboarding/KeyboardNav.tsx
+++ src/components/Onboarding/KeyboardNav.tsx
@@ -4,6 +4,26 @@ interface KeyboardNavConfig {
   keyEvents?: { [key: string]: (e: KeyboardEvent) => void };
 }
 
+/**
+ * Data selectors used by useKeyboardNavigation to implement navigation behavior.
+ */
+const DataSelector = {
+  /**
+   * All focusable elements will be navigable via the ArrowUp and ArrowDown keys. Setting the attribute equal to a string
+   * will allow you to tag the element with an id, which can be used by jumpTo to select an element directly on a keypress.
+   */
+  FOCUSABLE: '[data-focusable]',
+  /**
+   * The first child found with immediate focus will be focused when the component first mounts.
+   */
+  IMMEDIATE_FOCUS: '[data-immediate-focus]',
+};
+
+/**
+ * Allows users to navigate through the application using ArrowUp and ArrowDown.
+ * @param jumpTo maps key identifiers to data-focusable ids. When the key is pressed the element with the given id is focused.
+ * @param keyEvents maps key identifiers to event handlers.
+ */
 export function useKeyboardNavigation({
   jumpTo = {},
   keyEvents = {},
@@ -12,20 +32,24 @@ export function useKeyboardNavigation({
   const setFocusIndex = useState(0)[1];
 
   useEffect(() => {
-    const el = containerRef.current?.querySelector('[data-immediate-focus]') as
-      | HTMLElement
-      | undefined;
+    const el = containerRef.current?.querySelector(
+      DataSelector.IMMEDIATE_FOCUS
+    ) as HTMLElement | undefined;
     el?.focus();
   }, []);
 
   useEffect(() => {
-    function incrementElement(amount: number) {
+    /**
+     * Adjusts focus to a new element based on the step.
+     * @param step delta between the current value and the new focus index.
+     */
+    function incrementElement(step: number) {
       setFocusIndex((i) => {
         if (!containerRef.current) {
           return i;
         }
         const focusEls = containerRef.current.querySelectorAll(
-          '[data-focusable]'
+          DataSelector.FOCUSABLE
         ) as NodeListOf<HTMLElement>;
         if (!focusEls.length) {
           return i;
@@ -34,7 +58,7 @@ export function useKeyboardNavigation({
           el.classList.remove('focused');
           el.blur();
         });
-        const newI = Math.max(Math.min(focusEls.length - 1, i + amount), 0);
+        const newI = Math.max(Math.min(focusEls.length - 1, i + step), 0);
         focusEls[newI].focus();
         focusEls[newI].scrollIntoView({
           behavior: 'smooth',
@@ -45,12 +69,16 @@ export function useKeyboardNavigation({
       });
     }
 
+    /**
+     * Focuses an element with the given id.
+     * @param id The string value of data-focusable to find in the DOM.
+     */
     function jumpToElement(id: string) {
       if (!containerRef.current) {
         return;
       }
       const focusEls = Array.from(
-        containerRef.current.querySelectorAll('[data-focusable]')
+        containerRef.current.querySelectorAll(DataSelector.FOCUSABLE)
       ) as HTMLElement[];
       if (!focusEls.length) {
         return;
