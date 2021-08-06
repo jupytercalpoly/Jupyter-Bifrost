123dbd3ccd897b3d76ab5697ea8498c0259db9fe
jahn <aju960219@gmail.com>
2021-08-06 13:46:57 -0700

    Fixed chart filter issue

--- src/assets/icons/ChartIcons.ts
+++ src/assets/icons/ChartIcons.ts
@@ -2,10 +2,12 @@ import ScatterChartIcon from './ScatterChartIcon';
 import BarChartIcon from './BarChartIcon';
 import LineChartIcon from './LineChartIcon';
 import PieChartIcon from './PieChartIcon';
+import TickChartIcon from './TickChartIcon';
 
 export const chartIcons = [
   { icon: BarChartIcon, mark: 'bar' },
   { icon: ScatterChartIcon, mark: 'point' },
   { icon: LineChartIcon, mark: 'line' },
   { icon: PieChartIcon, mark: 'arc' },
+  { icon: TickChartIcon, mark: 'tick' },
 ];
