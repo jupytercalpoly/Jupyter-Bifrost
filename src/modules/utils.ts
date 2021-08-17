0784cb11b88d04269fcde70d7491cd05607ed927
jahn <aju960219@gmail.com>
2021-08-17 11:06:12 -0700

    Updated filter screen

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -1,2 +1,14 @@
+import { GraphSpec, EncodingInfo } from '../hooks/bifrost-model';
+
 export const isFunction = (val: any): val is Function =>
   val && {}.toString.call(val) === '[object Function]';
+
+export function hasDuplicateField(
+  graphSpec: GraphSpec,
+  field: string
+): boolean {
+  const countFieldUsage = Object.values(graphSpec.encoding).filter(
+    (encodingInfo: EncodingInfo) => encodingInfo.field === field
+  ).length;
+  return countFieldUsage !== 1;
+}
