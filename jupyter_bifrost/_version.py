d3f80f04f0254e7a4d8cbb52ef02c13ab95bcba7
John Waidhofer <33008436+Waidhoferj@users.noreply.github.com>
2021-08-27 10:27:32 -0700

    version update

--- jupyter_bifrost/_version.py
+++ jupyter_bifrost/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) John Waidhofer(waidhoferj), Jay Ahn(jahn96).
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 1, "dev")
+version_info = (0, 2, 2, "dev")
 __version__ = ".".join(map(str, version_info))
